d84597cb67d77ec754e81e1beae9583c1b842d54
Richard King <richrdkng@gmail.com>
2019-09-06 23:44:02 +0200

    test(cli): update help flag tests for the new formatted text
    Also add a getFile helper function to test/utils.

--- test/cli/help-flag.test.js
+++ test/cli/help-flag.test.js
@@ -3,28 +3,17 @@
 const _ = require('../utils')
 
 _.test('cli:help flag', async t => {
-  const indices = [1, 10, 80, 252]
+  const helpFile = await _.readFile(_.path.helpText)
+  const trimmedHelpFile = helpFile.slice(0, -1)
 
   const { stdout: helpShort } = await _.run(`node ${_.cli} -h`)
-  const helpShortIndices = [
-    helpShort.indexOf('Usage:'),
-    helpShort.indexOf('$ noop-server'),
-    helpShort.indexOf('Options:'),
-    helpShort.indexOf('Examples:')
-  ]
 
-  t.deepEqual(helpShortIndices, indices,
+  t.deepEqual(helpShort, trimmedHelpFile,
     '$ noop-server -h should return the help text')
 
   const { stdout: helpLong } = await _.run(`node ${_.cli} --help`)
-  const helpLongIndices = [
-    helpLong.indexOf('Usage:'),
-    helpLong.indexOf('$ noop-server'),
-    helpLong.indexOf('Options:'),
-    helpLong.indexOf('Examples:')
-  ]
 
-  t.deepEqual(helpLongIndices, indices,
+  t.deepEqual(helpLong, trimmedHelpFile,
     '$ noop-server --help should return the help text')
 
   t.end()
--- test/utils/index.js
+++ test/utils/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const fs = require('fs')
 const path = require('path')
 const test = require('tape')
 const execa = require('execa')
@@ -8,6 +9,8 @@ const fetch = require('node-fetch')
 const { JSDOM } = require('jsdom')
 
 const NoopServer = require('../../lib')
+const root = path.join(__dirname, '../../')
+const bin = path.join(root, '/bin')
 
 module.exports = {
   test,
@@ -15,8 +18,20 @@ module.exports = {
   fetch,
   JSDOM,
   run: execa.command,
+  readFile: async path =>
+    new Promise((resolve, reject) => {
+      fs.readFile(path, 'utf-8', (error, data) => {
+        if (error) reject(error)
+        resolve(data)
+      })
+    }),
   port: async () => getPort({ port: NoopServer.DEFAULT_PORT }),
   cli: path.resolve(__dirname, '../../bin/cli.js'),
   package: require('../../package.json'),
-  timeout: 1750 // in milliseconds
+  timeout: 1750, // in milliseconds
+  path: {
+    root,
+    bin,
+    helpText: path.join(bin, '/help.txt')
+  }
 }
